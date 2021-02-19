6cb288abded9f9a817613ea78da920fe77e725e3
彭道宽 <1063137960@qq.com>
2021-02-19 18:01:26 +0800

    [feature][what]终端参数配置类型添加

--- src/type/common.ts
+++ src/type/common.ts
@@ -1,3 +1,5 @@
+import * as vscode from 'vscode'
+
 // 工程文件类型
 export interface FolderType {
   name: string
@@ -13,7 +15,16 @@ export interface VscodeExtensionType {
 
 // 命令
 export interface ShellType {
-  key: string
-  value: string
-  environment?: string
+  key: string // 脚本名
+  value: string // 脚本内容
+  environment?: string // 脚本环境
+}
+
+// 终端参数配置
+export interface MyTerminalOptions extends vscode.TerminalOptions {
+  terminalCwd: string; // 执行目录
+  terminalName?: string; // 终端名称
+  terminalText?: string; // 终端填充内容
+  terminalAutoInputText?: boolean; // 是否自动填充终端内容
+  terminalAutoRun?: boolean; // 是否自动运行
 }
