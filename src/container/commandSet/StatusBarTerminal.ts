b2de4b8df592218b4ef321f4c7c51938fc4377e1
彭道宽 <pengdaokuan@cvte.com>
2021-02-20 18:10:35 +0800

    [feature][what]实现高配版本下的拆分终端功能

--- src/container/commandSet/StatusBarTerminal.ts
+++ src/container/commandSet/StatusBarTerminal.ts
@@ -5,39 +5,53 @@ import { MyTerminalOptions } from '../../type/common'
  * @description 终端
  * @property {number} terminalIndex 终端下标
  * @property {object} terminalOptions 配置参数
+ * @property {boolean} terminalCreate 默认生成 terminal 
  * 具体参数可阅读: https://code.visualstudio.com/api/references/vscode-api#TerminalOptions
  */
 export class StatusBarTerminal {
   private _item: vscode.StatusBarItem
-  private _terminal: vscode.Terminal
+  private _terminal: vscode.Terminal | undefined
+  public terminalName: string | undefined
+  public terminalIndex: number | undefined
 
-  constructor(terminalIndex: number, terminalOptions: MyTerminalOptions) {
+  constructor(terminalIndex: number, terminalOptions: MyTerminalOptions, terminalCreate:boolean = true) {
+    this.terminalIndex = terminalIndex
+    this.terminalName = terminalOptions?.terminalName
     this._item = vscode.window.createStatusBarItem()
     this.setTerminalIndex(terminalIndex)
     this._item.show()
 
-    this._terminal = vscode.window.createTerminal({
-      cwd: terminalOptions?.terminalCwd,
-      name: terminalOptions?.terminalName,
-    })
-
-    if (terminalOptions?.terminalAutoInputText) {
-      if (terminalOptions?.terminalText) {
-        this._terminal.sendText(
-          terminalOptions.terminalText,
-          terminalOptions.terminalAutoRun
-        )
+    if (terminalCreate) {
+      this._terminal = vscode.window.createTerminal({
+        cwd: terminalOptions?.terminalCwd,
+        name: terminalOptions?.terminalName,
+      })
+  
+      if (terminalOptions?.terminalAutoInputText) {
+        if (terminalOptions?.terminalText) {
+          this._terminal.sendText(
+            terminalOptions.terminalText,
+            terminalOptions.terminalAutoRun
+          )
+        }
       }
+      this._terminal.show()
     }
-    this._terminal.show()
   }
 
   public show(): void {
-    this._terminal.show()
+    this._terminal?.show()
   }
 
   public setTerminalIndex(index: number): void {
     this._item.text = `$(terminal)${index + 1}`
+  }
+
+  // 更新terminal信息（use split terminal）
+  public updateTerminal(terminal: vscode.Terminal | undefined) {
+    if (terminal) {
+      this._terminal = terminal
+    }
   }
 
   public hasTerminal(terminal: vscode.Terminal): boolean {
@@ -46,6 +60,6 @@ export class StatusBarTerminal {
 
   public dispose(): void {
     this._item.dispose()
-    this._terminal.dispose()
+    this._terminal?.dispose()
   }
 }
--- src/container/commandSet/index.ts
+++ src/container/commandSet/index.ts
@@ -1,14 +1,14 @@
 import * as vscode from 'vscode'
 import SideBarCommand from './SideBarCommand'
-import { ShellType } from '../../type/common'
-import { getWorkSpaceFolderList } from '../../utils'
+import { MyTerminalOptions, ShellType } from '../../type/common'
+import { uniqBy, getWorkSpaceFolderList } from '../../utils'
 import { PREFIX, MATCH_CONFIG_MAPS } from '../../constants'
 import { StatusBarTerminal } from './StatusBarTerminal'
 
 const MAX_TERMINALS = 10
-let terminals: StatusBarTerminal[] = [];
-let terminalCount = 0;
-let terminalIndex:number;
+let terminalCount = 0
+let terminalIndex: number
+let terminals: StatusBarTerminal[] = []
 
 module.exports = function (context: vscode.ExtensionContext) {
   // 得到vscode所有工程项目
@@ -18,48 +18,85 @@ module.exports = function (context: vscode.ExtensionContext) {
   const sideBar = new SideBarCommand(folderList)
   vscode.window.registerTreeDataProvider('BeeHive-Command', sideBar)
 
-  //注册命令
+  // 注册命令
   vscode.commands.registerCommand(
     'BeeHive-Command.openChild',
-    (args: { title: string; shell: ShellType; [key: string]: any }) => {
-      const { title, shell = null, path = '', projectName } = args
+    async (args: { title: string; shell: ShellType; [key: string]: any }) => {
+      const { title, shell = null, path, projectName } = args
       const reg = new RegExp(`${PREFIX}`)
       if (reg.test(title)) {
         vscode.window.showErrorMessage(title)
       } else {
-        // 获取用户配置的版本设置
-        const matchConfig = vscode.workspace.getConfiguration().get('vscode-beehive-extension.matchConfig')
+        // 获取用户配置的beehive版本设置
+        const matchConfig = vscode.workspace.getConfiguration().get('vscode-beehive-extension.matchConfig') || MATCH_CONFIG_MAPS.LOW
+        // 获取用户配置的是否分割终端设置
+        const splitTerminal = vscode.workspace.getConfiguration().get('vscode-beehive-extension.splitTerminal') || false;
+        // 获取用户配置的是否自动运行脚本
+        const autoRunTerminal: boolean = vscode.workspace.getConfiguration().get('vscode-beehive-extension.autoRunTerminal') || false
+
         if (matchConfig === MATCH_CONFIG_MAPS.LOW) {
-          // 低配：复制到剪切板
+          // 低配：脚本命令复制到剪切板
           vscode.env.clipboard.writeText(`cd ${path} \nnpm run ${shell?.key}`)
           vscode.window.showInformationMessage(`fine ! shell copied to clipboard ~`)
         } else if (matchConfig === MATCH_CONFIG_MAPS.MIDDLE) {
-          // 中配：打开终端，跳转当前项目，不自动运行
+          // 中配：打开终端，跳转当前项目，填充脚本，不自动运行
           if (terminals.length >= MAX_TERMINALS) {
-            vscode.window.showInformationMessage(`${PREFIX} does not support more than ${MAX_TERMINALS} terminals.`);
-            return;
+            vscode.window.showWarningMessage(`${PREFIX} does not support more than ${MAX_TERMINALS} terminals.`)
+            return
           }
-          terminals.push(new StatusBarTerminal(terminalCount++, {
-            terminalCwd: path,
-            terminalName: projectName,
-            terminalText: `npm run ${shell?.key}`,
-            terminalAutoInputText: true,
-            terminalAutoRun: false
-          }))
+          terminals.push(
+            new StatusBarTerminal(terminalCount++, {
+              terminalCwd: path,
+              terminalName: projectName,
+              terminalText: `npm run ${shell?.key}`,
+              terminalAutoInputText: true,
+              terminalAutoRun: autoRunTerminal
+            })
+          )
           context.subscriptions.push(vscode.window.onDidCloseTerminal(onDidCloseTerminal))
         } else if (matchConfig === MATCH_CONFIG_MAPS.HIGH) {
-          // 高配：支持分屏、自动运行项目、多项目终端切换
-          if (terminals.length >= MAX_TERMINALS) {
-            vscode.window.showInformationMessage(`${PREFIX} does not support more than ${MAX_TERMINALS} terminals.`);
-            return;
+          // 高配：填充脚本、支持分屏、自动运行项目、多项目终端切换
+          // 1.1 获取当前所有运行的终端数量（去重）
+          const uniqTerminals = uniqBy(terminals, 'terminalName')
+          // 1.2 尝试获取当前点击的项目脚本是否存在终端实例
+          const currentProjectTerminal = uniqTerminals.find((t) => t.terminalName === projectName)
+          // 1.3 如果当前的项目脚本并不存在终端实例，新增
+          if (!currentProjectTerminal) {
+            terminals.push(
+              new StatusBarTerminal(terminalCount++, {
+                terminalCwd: path,
+                terminalName: projectName,
+                terminalText: `npm run ${shell?.key}`,
+                terminalAutoInputText: true,
+                terminalAutoRun: autoRunTerminal
+              })
+            )
           }
-          terminals.push(new StatusBarTerminal(terminalCount++, {
-            terminalCwd: path,
-            terminalName: projectName,
-            terminalText: `npm run ${shell?.key}`,
-            terminalAutoInputText: true,
-            terminalAutoRun: true
-          }))
+          // 1.4 当前项目脚本存在终端实例
+          if (currentProjectTerminal) {
+            // 1.4.1 用户设置不需要分屏，则新增
+            if (!splitTerminal) {
+              terminals.push(
+                new StatusBarTerminal(terminalCount++, {
+                  terminalCwd: path,
+                  terminalName: projectName,
+                  terminalText: `npm run ${shell?.key}`,
+                  terminalAutoInputText: true,
+                  terminalAutoRun: autoRunTerminal
+                })
+              )
+            } else {
+              currentProjectTerminal?.show()
+              await createNewSplitTerminal(terminalCount++, {
+                terminalCwd: path,
+                terminalName: projectName,
+                terminalText: `npm run ${shell?.key}`,
+                terminalAutoInputText: true,
+                terminalAutoRun: autoRunTerminal
+              })
+            }
+          }
+
           context.subscriptions.push(vscode.window.onDidCloseTerminal(onDidCloseTerminal))
         } else {
           vscode.window.showErrorMessage(`unknown error`)
@@ -69,17 +106,38 @@ module.exports = function (context: vscode.ExtensionContext) {
   )
 }
 
-
+// 关闭终端
 function onDidCloseTerminal(terminal: vscode.Terminal): void {
   terminals.forEach((statusBarTerminal, index) => {
     if (statusBarTerminal.hasTerminal(terminal)) {
-      terminalIndex = index;
+      terminalIndex = index
     }
   })
-  terminals[terminalIndex].dispose();
-  terminals.splice(terminalIndex, 1);
+  terminals[terminalIndex]?.dispose()
+  terminals.splice(terminalIndex, 1)
   terminals.forEach((statusBarTerminal, i) => {
-      terminals[i].setTerminalIndex(i);
-  });
-  terminalCount--;
+    terminals[i].setTerminalIndex(i)
+  })
+  terminalCount--
+}
+
+// 创建分割拆分终端
+async function createNewSplitTerminal(terminalIndex: number, terminalOptions: MyTerminalOptions): Promise<vscode.Terminal> {
+  return new Promise(async () => {
+    // 通过命令创建的终端是默认的终端信息，暂未发现此命令可以通过传参配置生成的命令
+    // 解决方案就是构造一个StatusBarTerminal实例，再updateTerminal
+    await vscode.commands.executeCommand('workbench.action.terminal.split')
+    const activeTerminal = vscode.window.activeTerminal
+    const splitInstance = new StatusBarTerminal(terminalIndex, terminalOptions, false)
+    if (activeTerminal && terminalOptions?.terminalAutoInputText) {
+      if (terminalOptions?.terminalText) {
+        activeTerminal.sendText(
+          terminalOptions.terminalText,
+          terminalOptions.terminalAutoRun
+        )
+      }
+    }
+    splitInstance.updateTerminal(activeTerminal)
+    terminals.push(splitInstance)
+  })
 }
